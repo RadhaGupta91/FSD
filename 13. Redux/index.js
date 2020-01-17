96e9b96942595c3eaa1b64d247e7882b364b66bc
radha <radha94646@gmail.com>
2020-01-17 17:12:54 +0530

    redux standalone

--- 13. Redux/index.js
+++ 13. Redux/index.js
@@ -1,24 +1,34 @@
+//Add redux
 const redux = require('redux');
+//create redux store to hold all states
 const createStore = redux.createStore;
+
+//initialize default state
 const initialState = {
     counter: 0
 }
-// Reducer
+
+/* Reducer to implement logic
+    @param state: Help to update state
+    @param action: will help to decide logic inside funciton 
+*/
 const rootReducer = (state = initialState, action) => {
     if (action.type === 'INC_COUNTER') {
+        
         return {
-            ...state,
-            counter: state.counter + 1
+            ...state, //copy state first
+            counter: state.counter + 1 //udpate state
         };
     }
     if (action.type === 'ADD_COUNTER') {
         return {
-            ...state,
-            counter: state.counter + action.value
+            ...state, //copy state first
+            counter: state.counter + action.value  //udpate state
         };
     }
     return state;
 };
+
 // Store
 const store = createStore(rootReducer);
 console.log(store.getState());
